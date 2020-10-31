30f3d3b0d19a96de7a07ee4ce48c13ad1910152b
Dmitry Cherkasov <43281522+KartonDCP@users.noreply.github.com>
2020-10-31 07:42:47 -0700

    update code

---
+++ CorrelationApp/WindowsFormsApp1/Math/Statistics/Correlations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Math.Statistics
+{
+    public static class Correlations
+    {
+        public static double Spearmanr(double[] rankX, double[] rankY)
+        {
+            int n = rankX.Length;
+
+            double sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += (rankX[i] - rankY[i]) * (rankX[i] - rankY[i]);
+            }
+            
+            return 1 - (6 * sum) / (n*n*n - n);
+        }
+    }
+}
---
+++ CorrelationApp/WindowsFormsApp1/Utils/ListExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WindowsFormsApp1.Utils
+{
+    public static class ListExtensions
+    {
+        public static int[] StandardRank(IEnumerable<double> data)
+        {
+            return data.AsParallel()
+                .Select((x, i) => new {OldIndex = i, Value = x, NewIndex = -1})
+                .OrderByDescending(x => x.Value)
+                .Select((x, i) => new {OldIndex = x.OldIndex, Value = x.Value, NewIndex = i + 1})
+                .OrderBy(x => x.OldIndex).Select((arg => arg.NewIndex))
+                .ToArray();
+        }
+
+        
+    }
+}
