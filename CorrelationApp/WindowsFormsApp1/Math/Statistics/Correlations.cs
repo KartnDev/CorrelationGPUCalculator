e5f53325ef034650c95f4b989ac606ad394066db
Dmitry Cherkasov <43281522+KartonDCP@users.noreply.github.com>
2020-10-31 08:19:45 -0700

    parallel for

--- CorrelationApp/WindowsFormsApp1/Math/Statistics/Correlations.cs
+++ CorrelationApp/WindowsFormsApp1/Math/Statistics/Correlations.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.IO;
 using System.Linq;
 using System.Threading;
@@ -11,16 +12,18 @@ namespace WindowsFormsApp1.Math.Statistics
         public static double Spearmanr(double[] rankX, double[] rankY)
         {
             double n = rankX.Length;
+            var bag = new ConcurrentBag<double>();
+            ParallelOptions po = new ParallelOptions();
+            Console.WriteLine(po.MaxDegreeOfParallelism);
             
-            
-            double sum = 0.0;
-            Parallel.For(0, (int)n, i =>
+            Parallel.For(0, (int)n,new ParallelOptions { MaxDegreeOfParallelism = 24 } ,i =>
             {
                 double temp = rankX[i] - rankY[i];
-                sum += System.Math.Pow(temp, 2);
+                bag.Add(System.Math.Pow(temp, 2));
+                
             });
-            
-            return 1.0 - (6.0 * sum) / (n*n*n -  n);
+
+            return 1.0 - (6.0 * bag.Sum()) / (n*n*n -  n);
         }
     }
 }
