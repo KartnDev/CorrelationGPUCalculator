c5d06036d69c79289653dcc09e59720ea48b22d2
Dmitry Cherkasov <43281522+KartonDCP@users.noreply.github.com>
2020-10-31 07:59:09 -0700

    fix correlation

--- CorrelationApp/WindowsFormsApp1/Math/Statistics/Correlations.cs
+++ CorrelationApp/WindowsFormsApp1/Math/Statistics/Correlations.cs
@@ -10,16 +10,17 @@ namespace WindowsFormsApp1.Math.Statistics
     {
         public static double Spearmanr(double[] rankX, double[] rankY)
         {
-            int n = rankX.Length;
-
-            double sum = 0;
-
-            for (int i = 0; i < n; i++)
-            {
-                sum += (rankX[i] - rankY[i]) * (rankX[i] - rankY[i]);
-            }
+            double n = rankX.Length;
             
-            return 1 - (6 * sum) / (n*n*n - n);
+            
+            double sum = 0.0;
+            Parallel.For(0, (int)n, i =>
+            {
+                double temp = rankX[i] - rankY[i];
+                sum += System.Math.Pow(temp, 2);
+            });
+            
+            return 1.0 - (6.0 * sum) / (n*n*n -  n);
         }
     }
 }
