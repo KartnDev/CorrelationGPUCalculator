4576339922cf165befd51bac384f9f99b9cd59c4
Dmitry Cherkasov <43281522+KartonDCP@users.noreply.github.com>
2020-11-01 04:32:12 -0800

    finish openblas

--- CorrelationApp/WindowsFormsApp1/DeviceComputes/CUDACompute.cs
+++ CorrelationApp/WindowsFormsApp1/DeviceComputes/CUDACompute.cs
@@ -1,7 +1,19 @@
-﻿namespace WindowsFormsApp1.DeviceComputes
+﻿using System.Collections.Generic;
+
+namespace WindowsFormsApp1.DeviceComputes
 {
-    public class CUDACompute
+    public class CUDACompute: ResultWrapper, IComputeDevice
     {
-        
+        public CUDACompute(string outputFolder, string prevName) : base(outputFolder, prevName)
+        {
+        }
+
+        public void ShiftCompute(List<double[]> fullSignals, int shiftWidth, int batchSize)
+        {
+            
+        }
+
+        public int MaxParallelDegree { get; set; }
+        public int RoundValue { get; set; }
     }
 }
