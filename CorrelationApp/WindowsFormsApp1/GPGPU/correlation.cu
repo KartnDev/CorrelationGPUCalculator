74718313a2f71517da12c98ea874a1664afb6e19
Dmitry Cherkasov <43281522+KartonDCP@users.noreply.github.com>
2020-11-03 11:47:44 -0800

    fix matrix correlation!

--- CorrelationApp/WindowsFormsApp1/GPGPU/correlation.cu
+++ CorrelationApp/WindowsFormsApp1/GPGPU/correlation.cu
@@ -1,129 +1,79 @@
-#include "cuda.h"
 #include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include <cuda.h>
 #include <iostream>
 #include <time.h>
 
-__global__ void dot_product_kernel(float *x, float *y, float *num, float *denom, unsigned int n)
+__global__ void correlation(float *x, float *y, float *num, float *denom, unsigned int n, float avg_x, float avg_y)
 {
-	unsigned int index = threadIdx.x + blockDim.x*blockIdx.x;
+    unsigned int index = threadIdx.x + blockDim.x*blockIdx.x;
 	unsigned int stride = blockDim.x*gridDim.x;
 
-    // Calculating avg of vectors
-    __shared__ float avg_x[256]; 
-    __shared__ float avg_y[256]; 
+    __shared__ float sum_x[256];
+    __shared__ float sum_y[256];
+    __shared__ float sum_pairwise[256];
+
 
     double temp_x = 0.0;
     double temp_y = 0.0;
+    double temp_pairwise = 0.0;
 
     while(index < n)
     {
-        temp_x += x[index];
-        temp_y += y[index];
+        temp_x += (x[index] - avg_x) * (x[index] - avg_x);
+        temp_y += (y[index] - avg_y) * (y[index] - avg_y);
+        temp_pairwise += (x[index] - avg_x) * (y[index] - avg_y);
 
 		index += stride;
-    }
-    avg_x[threadIdx.x] = temp_x;
-    avg_y[threadIdx.x] = temp_y;
-
-    __syncthreads();
-
-    unsigned int i = blockDim.x/2;
-    while(i != 0)
-    {
-        if(threadIdx.x < i)
-        {
-            avg_x[threadIdx.x] += avg_x[threadIdx.x + i];
-            avg_y[threadIdx.x] += avg_y[threadIdx.x + i];
-        }
-        __syncthreads();
-        i /= 2;
-    }
-
-    if(threadIdx.x == 0)
-    {
-        avg_x[0] = avg_x[0] / n;
-        avg_y[0] = avg_y[0] / n;
 	}
 
-    // calculation denominator x error sum
-    index = threadIdx.x + blockDim.x*blockIdx.x;
-    stride = blockDim.x*gridDim.x;
-    
-    __shared__ float sum_error_x[256]; 
-    __shared__ float sum_error_y[256]; 
-
-    temp_x = 0.0;
-    temp_y = 0.0;
-
-    while(index < n)
-    {
-        temp_x += (x[index] - avg_x[0]) * (x[index] - avg_x[0]);
-        temp_y += (y[index] - avg_y[0]) * (y[index] - avg_y[0]);
-
-	    index += stride;
-    }
-    sum_error_x[threadIdx.x] = temp_x;
-    sum_error_y[threadIdx.x] = temp_y;
-
-    __syncthreads();
-
-    i = blockDim.x/2;
-    while(i != 0)
-    {
-        if(threadIdx.x < i)
-        {
-            sum_error_x[threadIdx.x] += sum_error_x[threadIdx.x + i];
-            sum_error_y[threadIdx.x] += sum_error_y[threadIdx.x + i];
-        }
-        __syncthreads();
-        i /= 2;
-    }
-
-
-    // final reduction
-    index = threadIdx.x + blockDim.x*blockIdx.x;
-	stride = blockDim.x*gridDim.x;
-    
-	__shared__ float cache[256];
-
-	double temp = 0.0;
-    while(index < n)
-    {
-	    temp += (x[index] - avg_x[0]) * (y[index] - avg_y[0]);
-	    index += stride;
-	}
-
-	cache[threadIdx.x] = temp;
+    sum_x[threadIdx.x] = temp_x;
+    sum_y[threadIdx.x] = temp_y;
+    sum_pairwise[threadIdx.x] = temp_pairwise;
 
 	__syncthreads();
 
 	// reduction
-	i = blockDim.x/2;
+	unsigned int i = blockDim.x/2;
     while(i != 0)
     {
         if(threadIdx.x < i)
         {
-	        cache[threadIdx.x] += cache[threadIdx.x + i];
-	    }
-	    __syncthreads();
-	    i /= 2;
-    }
-    
+            sum_x[threadIdx.x] += sum_x[threadIdx.x + i];
+            sum_y[threadIdx.x] += sum_y[threadIdx.x + i];
+            sum_pairwise[threadIdx.x] += sum_pairwise[threadIdx.x + i];
+		}
+		__syncthreads();
+		i /= 2;
+	}
+
 
     if(threadIdx.x == 0)
     {
-        atomicAdd(denom, sqrtf(sum_error_x[0] * sum_error_y[0]));
-        atomicAdd(num, cache[0]);
+        atomicAdd(denom, sqrtf(sum_x[0] * sum_y[0]));
+        atomicAdd(num, sum_pairwise[0]);
 	}
 }
 
-float* gpgpu_mat_correlation(float** host_vectors, int count_of_vector, int n)
+double mean(float* data, int n)
+{
+    double sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        sum += data[i];
+    }
+    return sum / n;
+}
+
+float* gpgpu_correlation_mat(float** signals, int n, int signal_count)
 {
     dim3 gridSize = 256;
     dim3 blockSize = 256;
 
-    float *result = (float*)malloc(count_of_vector * count_of_vector * sizeof(float));   
-    float *temp = (float*)malloc(sizeof(float));
+    float* result = (float*)malloc(signal_count * signal_count * sizeof(float));
+
+    float *denom_res = (float*)malloc(sizeof(float));   
+    float *num_res = (float*)malloc(sizeof(float));
 
     float *d_prod_num, *d_prod_denom;
     float *d_x, *d_y;
@@ -133,27 +83,29 @@ float* gpgpu_mat_correlation(float** host_vectors, int count_of_vector, int n)
     cudaMalloc((void**)&d_prod_denom, sizeof(float));
     cudaMalloc((void**)&d_x, n*sizeof(float));
     cudaMalloc((void**)&d_y, n*sizeof(float));
-      
-    
-    for(int i =0; i < count_of_vector; i++)
+
+    float x_mean, y_mean;
+
+    for(int i = 0; i < signal_count; i++)
     {
-        for(int j =0; j < count_of_vector; j++)
-        {
-            cudaMemset(d_prod_num, 0.0, sizeof(float));
-            cudaMemset(d_prod_denom, 0.0, sizeof(float));
-            cudaMemcpy(d_x, (void*)host_vectors[i], n*sizeof(float), cudaMemcpyHostToDevice);
-            cudaMemcpy(d_y, (void*)host_vectors[j], n*sizeof(float), cudaMemcpyHostToDevice);
-        
-        
-            dot_product_kernel<<<gridSize, blockSize>>>(d_x, d_y, d_prod_num, d_prod_denom, n);
-            cudaMemcpy(&result[i * count_of_vector + j], d_prod_num, sizeof(float), cudaMemcpyDeviceToHost);
-            
-            cudaMemcpy(temp, d_prod_denom, sizeof(float), cudaMemcpyDeviceToHost);
-            result[i * count_of_vector + j] *= powf(*temp, -1);
+        for(int j = 0; j < signal_count; j++)
+        {   
+            cudaMemset(d_prod_num, 0.0f, sizeof(float));
+            cudaMemset(d_prod_denom, 0.0f, sizeof(float));
+            cudaMemcpy(d_x, (void*)signals[i], n*sizeof(float), cudaMemcpyHostToDevice);
+            cudaMemcpy(d_y, (void*)signals[j], n*sizeof(float), cudaMemcpyHostToDevice);
+
+            x_mean = mean(signals[i], n);
+            y_mean = mean(signals[j], n);
+
+            correlation<<<gridSize, blockSize>>>(d_x, d_y, d_prod_num, d_prod_denom, n, x_mean, y_mean);
+            cudaMemcpy(num_res, d_prod_num, sizeof(float), cudaMemcpyDeviceToHost);
+            cudaMemcpy(denom_res, d_prod_denom, sizeof(float), cudaMemcpyDeviceToHost);
+
+            result[i * signal_count + j] = (*num_res) / (*denom_res);
         }
     }
-    
-    
+
     return result;
 }
 
@@ -161,9 +113,8 @@ float* gpgpu_mat_correlation(float** host_vectors, int count_of_vector, int n)
 
 int main()
 {
-    srand( (unsigned)time(NULL) );
-    unsigned int n = 1 * 256 * 256;
-    int signal_count = 2;
+    unsigned int n = 256 * 256;
+    int signal_count = 7;
         
 
     float** h_x = (float**)malloc(signal_count*sizeof(float*));
@@ -181,17 +132,17 @@ int main()
         }
     }
 
-    
-    float* res = gpgpu_mat_correlation(h_x, signal_count, n);
-    
-    for(int i =0; i < signal_count; i++)
+    float * result = gpgpu_correlation_mat(h_x, n, signal_count);
+
+    for(int i = 0; i < signal_count; i++)
     {
-        for(int j =0; j < signal_count; j++)
-        {
-            std::cout << res[i * signal_count + j] << "\t\t";
+        for(int j = 0; j < signal_count; j++)
+        {   
+            printf("%.2f\t|\t", result[i * signal_count + j]);
         }
         std::cout << "\n";
     }
+    
     
     system("pause");
 }
